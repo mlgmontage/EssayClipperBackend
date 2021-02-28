43f5ade304399211b690d5a41b53e501962874ff
mlgmontage <kettleofrassel1@gmail.com>
2021-02-28 12:51:34 +0500

    setup monk & snipets list route

--- api/routes/snipets.js
+++ api/routes/snipets.js
@@ -1,8 +1,22 @@
 const { Router } = require("express");
 const router = Router();
+const db = require("monk")("localhost/EssayClipper");
+const snipets = db.get("snipets");
+snipets.createIndex("title");
 
-router.get("/", (req, res) => {
-  res.json({ message: "snipets" });
+/**
+ * Data model
+ *
+ * {
+ *    title,
+ *    markdown,
+ *    isArchived
+ * }
+ */
+
+router.get("/", async (req, res) => {
+  const snipetsList = await snipets.find({ isArchived: false });
+  res.json(snipetsList);
 });
 
 module.exports = router;
